763c58d82de7d2effb3386a14aa7b46d71aa6409
senafathoni2998 <senaahma2998@gmail.com>
2025-08-03 11:59:06 +0700

    Add custom hook useHttpClient for handling HTTP requests with loading and error states

---
+++ src/shared/hooks/http-hooks.js
@@ -0,0 +1,49 @@
+import { useCallback, useState, useRef, useEffect } from "react";
+
+const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeHttpRequests = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setIsLoading(true);
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCtrl.signal,
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message || "Failed to fetch data");
+        }
+
+        return responseData;
+      } catch (err) {
+        console.error(err);
+        setError(err.message);
+      }
+      setIsLoading(false);
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
+};
