ff8518714863bca835ef02679617f9eb91d790ee
senafathoni2998 <senaahma2998@gmail.com>
2025-08-03 11:27:37 +0700

    Implement user fetching and loading state in Users component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,17 +1,49 @@
+import React, { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
-
-const USERS = [
-  {
-    id: "u1",
-    name: "Max Schwarz",
-    image:
-      "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    places: 3,
-  },
-];
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
-  return <UsersList items={USERS} />;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [loadedUsers, setLoadedUsers] = useState([]);
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message || "Failed to fetch users data");
+        }
+
+        setLoadedUsers(responseData.users || []);
+      } catch (err) {
+        console.error(err);
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  console.log(isLoading);
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && <LoadingSpinner asOverlay />}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
 };
 
 export default Users;
