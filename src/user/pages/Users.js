683d6007238a576be5aa0ccfdedd2277795c1ffb
senafathoni2998 <senaahmad2998@gmail.com>
2025-08-03 12:52:12 +0700

    Refactor Users component to utilize useHttpClient for improved data fetching and error handling

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -2,44 +2,27 @@ import React, { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import useHttpClient from "../../shared/hooks/http-hooks";
 
 const Users = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [loadedUsers, setLoadedUsers] = useState([]);
 
   useEffect(() => {
-    const sendRequest = async () => {
-      setIsLoading(true);
+    const getUsers = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/users");
-
-        const responseData = await response.json();
-
-        if (!response.ok) {
-          throw new Error(responseData.message || "Failed to fetch users data");
-        }
+        const responseData = sendRequest("http://localhost:5000/api/users");
 
         setLoadedUsers(responseData.users || []);
-      } catch (err) {
-        console.error(err);
-        setError(err.message);
-      }
-      setIsLoading(false);
+      } catch (err) {}
     };
 
-    sendRequest();
-  }, []);
-
-  const errorHandler = () => {
-    setError(null);
-  };
-
-  console.log(isLoading);
+    getUsers();
+  }, [sendRequest]);
 
   return (
     <React.Fragment>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && <LoadingSpinner asOverlay />}
       {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
     </React.Fragment>
