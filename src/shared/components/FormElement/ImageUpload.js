5d5c743134b53915b163b71c8e44f7e199417838
senafathoni2998 <senaahmad2998@gmail.com>
2025-08-04 21:00:06 +0700

    Enhance ImageUpload component with file handling and preview functionality

--- src/shared/components/FormElement/ImageUpload.js
+++ src/shared/components/FormElement/ImageUpload.js
@@ -1,12 +1,33 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import "./ImageUpload.css";
 
 const ImageUpload = (props) => {
   const filePickerRef = useRef();
+  const [file, setFile] = useState();
+  const [previewUrl, setPreviewUrl] = useState();
+  const [isValid, setIsValid] = useState(false);
+
+  useEffect(() => {
+    if (!file) return;
+
+    const fileReviewUrl = new FileReader();
+    fileReviewUrl.onload = () => {
+      setPreviewUrl(fileReviewUrl.result);
+    };
+    fileReviewUrl.readAsDataURL(file);
+  }, [file]);
 
   const pickedHandler = (event) => {
-    console.log(event.target);
+    let pickedFile;
+    let fileIsValid = isValid;
+    if (event.target.files && event.target.files.length === 1) {
+      const pickedFile = event.target.files[0];
+      setFile(pickedFile);
+      fileIsValid = true;
+    }
+    setIsValid(fileIsValid);
+    props.onInput(props.id, pickedFile, fileIsValid);
   };
 
   const pickImageHandler = () => {
@@ -25,12 +46,14 @@ const ImageUpload = (props) => {
       />
       <div className={`image-upload ${props.center && "center"}`}>
         <div className="image-upload__preview">
-          <img src="" alt="Preview" />
+          {previewUrl && <img src={previewUrl} alt="Preview" />}
+          {!previewUrl && <p>Please pick an image.</p>}
         </div>
         <Button type="button" onClick={pickImageHandler}>
           PICK IMAGE
         </Button>
       </div>
+      {!isValid && <p>{props.error}</p>}
     </div>
   );
 };
