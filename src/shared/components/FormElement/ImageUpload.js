642ee271ab42bcaa0024b4d9374ea852c66e1b12
senafathoni2998 <senaahmad2998@gmail.com>
2025-08-04 20:31:56 +0700

    Add ImageUpload component for handling image file uploads

---
+++ src/shared/components/FormElement/ImageUpload.js
@@ -0,0 +1,38 @@
+import { useRef } from "react";
+import Button from "./Button";
+import "./ImageUpload.css";
+
+const ImageUpload = (props) => {
+  const filePickerRef = useRef();
+
+  const pickedHandler = (event) => {
+    console.log(event.target);
+  };
+
+  const pickImageHandler = () => {
+    filePickerRef.current.click();
+  };
+
+  return (
+    <div className="form-control">
+      <input
+        ref={filePickerRef}
+        id={props.id}
+        style={{ display: "none" }}
+        type="file"
+        accept=".jpg,.png,.jpeg"
+        onChange={pickedHandler}
+      />
+      <div className={`image-upload ${props.center && "center"}`}>
+        <div className="image-upload__preview">
+          <img src="" alt="Preview" />
+        </div>
+        <Button type="button" onClick={pickImageHandler}>
+          PICK IMAGE
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ImageUpload;
