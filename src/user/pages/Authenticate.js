f39158bf3a3f8c65a2c1c9632d8a70289e4f710b
senafathoni2998 <senaahmad2998@gmail.com>
2025-08-04 21:27:37 +0700

    Refactor signup request to use FormData for handling image uploads

--- src/user/pages/Authenticate.js
+++ src/user/pages/Authenticate.js
@@ -59,17 +59,16 @@ const Authenticate = () => {
       } catch (err) {}
     } else {
       try {
+        const formData = new FormData();
+        formData.append("email", formState.inputs.email.value);
+        formData.append("name", formState.inputs.name.value);
+        formData.append("password", formState.inputs.password.value);
+        formData.append("image", formState.inputs.image.value);
+        console.log(formState.inputs);
         const responseData = await sendRequest(
           "http://localhost:5000/api/users/signup",
           "POST",
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value,
-          }),
-          {
-            "Content-Type": "application/json",
-          }
+          formData
         );
 
         console.log(responseData.user);
