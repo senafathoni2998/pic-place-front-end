22b83cb7c9214b716bdea3b81417cdefebf7f4c3
senafathoni2998 <senaahmad2998@gmail.com>
2025-08-10 00:49:55 +0700

    Add error message for image upload in Authenticate component

--- src/user/pages/Authenticate.js
+++ src/user/pages/Authenticate.js
@@ -160,7 +160,12 @@ const Authenticate = () => {
             />
           )}
           {!isLoginMode && (
-            <ImageUpload id="image" center onInput={inputHandler} />
+            <ImageUpload
+              id="image"
+              center
+              onInput={inputHandler}
+              errorText="Please provide an image"
+            />
           )}
           <Input
             element="input"
